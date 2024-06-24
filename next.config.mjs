f31f6f26ccff5c4957518974a4359227bb1cb168
jilla pavan <jillapavan22001@gmail.com>
2024-06-24 19:16:26 +0530

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
